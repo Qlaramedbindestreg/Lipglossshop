b865fa6e66d09096e6de0b6ada0eb386219d97c6
Qlaramedbindestreg <u2470787@rts-365.dk>
2025-01-17 08:31:39 +0100

    add staged changes

--- sass-webshop-Qlaramedbindestreg/js/components/product.js
+++
@@ -1,9 +0,0 @@
-const PRODUCTS_URL = './public/assets/products.json';
-
-export async function getProducts() {
-    const response = await fetch(PRODUCTS_URL);
-    const data = await response.json();
-
-    return data;
-}
-
